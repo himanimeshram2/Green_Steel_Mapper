ea194412df0a2048a38db5052c0e4af74fcf65b2
himanimeshram2 <himanimeshram2@gmail.com>
2024-07-10 20:24:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize the map
+    var map = L.map('map').setView([-25.2744, 133.7751], 4);
+
+    // Add OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Example marker
+    var marker = L.marker([-25.2744, 133.7751]).addTo(map)
+        .bindPopup('Example Marker')
+        .openPopup();
+
+    document.getElementById('select-point').addEventListener('click', function() {
+        alert('Select Point functionality will be implemented here.');
+    });
+});
